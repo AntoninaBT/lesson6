eda2e6ffedf42f1d4c2c0984c46cd8588c28f84b
AntoninaBT <gorbaa533@gmail.com>
2024-10-13 19:27:48 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,14 @@
+# Teplova // Create a variable with "input". Name and display it.
+# Count the symbols of the entered text. I found and used "len"
+my_string = input("Sum of money (in words): ")
+print(len(my_string))
+# Output the entered text in uppercase. Use method: ".upper()"
+print(my_string.upper())
+# Output the entered text in lowercase. Use method: ".lower()"
+print(my_string.lower())
+# Delete all the gaps in the variable. Use method: ".replace()"
+print(my_string.replace(" ",""))
+# Output the first symbol of the string
+print(my_string[0])
+# Output the last symbol of the string
+print(my_string[-1])
